3807c5fc0417f8f30c34f2bce94c53661bcdd2ab
Franco-Massola <117114722+Franco-Massola@users.noreply.github.com>
2024-11-04 19:26:52 -0300

    carrusel detalles

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const images = [
 let currentIndex = 0;
 
 function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-slide');
+    const slides = document.querySelectorAll('.carrusel-slide');
     slides.forEach((slide, i) => {
         slide.classList.remove('active'); // Elimina la clase activa de todas las imágenes
         if (i === index) {
@@ -17,13 +17,13 @@ function showSlide(index) {
 }
 
 function nextSlide() {
-    const slides = document.querySelectorAll('.carousel-slide');
+    const slides = document.querySelectorAll('.carrusel-slide');
     currentIndex = (currentIndex + 1) % slides.length; // Circula al siguiente
     showSlide(currentIndex);
 }
 
 function prevSlide() {
-    const slides = document.querySelectorAll('.carousel-slide');
+    const slides = document.querySelectorAll('.carrusel-slide');
     currentIndex = (currentIndex - 1 + slides.length) % slides.length; // Circula al anterior
     showSlide(currentIndex);
 }
